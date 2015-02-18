07b36026dcf0d4934a5b491a1cc2cf6f41ffafdb
qube81 <yuta.tkhs@gmail.com>
2015-02-18 23:33:23 +0900

    use goroutine

--- bgm.go
+++ bgm.go
@@ -4,36 +4,52 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
 )
 
-var MatsukenSamba = "http://a588.phobos.apple.com/us/r20/Music/v4/82/a0/27/82a02731-c7f8-fec1-c686-dad12eca272f/mzaf_8898382402336846733.m4a"
-
 func main() {
 
-	response, err := http.Get(MatsukenSamba)
-	if err != nil {
-		panic(err)
-	}
-	defer response.Body.Close()
+	var MatsukenSamba = "http://a588.phobos.apple.com/us/r20/Music/v4/82/a0/27/82a02731-c7f8-fec1-c686-dad12eca272f/mzaf_8898382402336846733.m4a"
 
-	fmt.Println("status:", response.Status)
+	filename := <-GetMusicFile(MatsukenSamba)
+	defer os.Remove(filename)
 
-	file, err := ioutil.TempFile(os.TempDir(), "tmp_bgm_")
-	if err != nil {
-		panic(err)
-	}
-
-	defer file.Close()
-	defer os.Remove(file.Name())
-
-	fmt.Print("▶️　Playing Matsuken Samba💃")
-
-	io.Copy(file, response.Body)
-
-	out, _ := exec.Command("afplay", file.Name(), "-r", "1").CombinedOutput()
+	fmt.Println("NOW PLAYING: Matsuken Samba 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃")
+	out, _ := exec.Command("afplay", filename, "-r", "1").CombinedOutput()
 	fmt.Print(string(out))
+}
+
+/*
+GetMusicFile download specified url music file, and save temp dir
+@return channel tempfile name
+*/
+func GetMusicFile(url string) <-chan string {
+
+	fileNameChan := make(chan string)
+
+	go func(url string) {
+		fmt.Print("Loading...")
+		response, err := http.Get(url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer response.Body.Close()
+
+		file, err := ioutil.TempFile(os.TempDir(), "tmp_bgm_")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		fmt.Println("http status: " + response.Status)
+		io.Copy(file, response.Body)
+		fileNameChan <- file.Name()
+
+	}(url)
+
+	return fileNameChan
 
 }
