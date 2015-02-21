550dd6b3b37f528fb02872772be967e44bd41027
qube81 <yuta.tkhs@gmail.com>
2015-02-21 14:48:43 +0900

    get itunes json

--- bgm.go
+++ bgm.go
@@ -8,25 +8,60 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
 )
 
 func main() {
 
-	var MatsukenSamba = "http://a588.phobos.apple.com/us/r20/Music/v4/82/a0/27/82a02731-c7f8-fec1-c686-dad12eca272f/mzaf_8898382402336846733.m4a"
+	var (
+		argsLen       = len(os.Args)
+		rate          = 1
+		err           error
+		search        string
+		MatsukenSamba = "http://a989.phobos.apple.com/us/r20/Music2/v4/a2/b5/1b/a2b51beb-4b0a-a9a2-a636-03c215ab2db3/mzaf_3999292651922270843.aac.m4a"
+	)
 
-	filename := <-GetMusicFile(MatsukenSamba)
+	for i := 0; i < argsLen; i++ {
+
+		if i == 0 {
+			continue
+		}
+
+		v := os.Args[i]
+
+		if i == 1 {
+			search = v
+		}
+
+		if i+1 < argsLen {
+			if v == "--rate" || v == "-r" {
+				rate, err = strconv.Atoi(os.Args[i+1])
+				if err != nil {
+					rate = 1
+				}
+				i++
+			}
+		}
+
+	}
+
+	json := <-RequestItunes(search)
+	fmt.Println(json)
+
+	filename := <-Download(MatsukenSamba)
 	defer os.Remove(filename)
 
-	fmt.Println("NOW PLAYING: Matsuken Samba 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃 💃")
-	out, _ := exec.Command("afplay", filename, "-r", "1").CombinedOutput()
+	fmt.Println("NOW PLAYING: {#artistname} - {#trackname}")
+	out, _ := exec.Command("afplay", filename, "--rate", strconv.Itoa(rate)).CombinedOutput()
 	fmt.Print(string(out))
+
 }
 
 /*
 GetMusicFile download specified url music file, and save temp dir
 @return channel tempfile name
 */
-func GetMusicFile(url string) <-chan string {
+func Download(url string) <-chan string {
 
 	fileNameChan := make(chan string)
 
@@ -52,4 +87,31 @@ func GetMusicFile(url string) <-chan string {
 
 	return fileNameChan
 
+}
+
+/*
+
+*/
+func RequestItunes(term string) <-chan string {
+	jsonChan := make(chan string)
+	itunesEndPoint := "https://itunes.apple.com/search?term=%s&country=JP&media=music&entity=song&attribute=songTerm&limit=1"
+
+	go func(url string) {
+		fmt.Print("Request Itunes...")
+		response, err := http.Get(url)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			defer response.Body.Close()
+			contents, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				fmt.Printf("%s", err)
+				os.Exit(1)
+			}
+			jsonChan <- string(contents)
+		}
+
+	}(fmt.Sprintf(itunesEndPoint, term))
+
+	return jsonChan
 }
